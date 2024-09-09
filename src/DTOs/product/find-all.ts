0168033693136553222ead88deb8b1e38e9c9b2f
titi-lima <titisau@gmail.com>
2024-09-09 12:26:58 -0300

    fix: coerce query strings to number

--- src/DTOs/product/find-all.ts
+++ src/DTOs/product/find-all.ts
@@ -3,10 +3,12 @@ import { z } from 'zod';
 export const FindAllProductWhereDTO = z.object({
   name: z.string().optional(),
   description: z.string().optional(),
-  available: z.boolean().optional(),
+  available: z.coerce.boolean().optional(),
   priceOrderBy: z.enum(['asc', 'desc']).optional(),
-  limit: z.number({ invalid_type_error: 'Limit must be a number' }).optional(),
-  offset: z
+  limit: z.coerce
+    .number({ invalid_type_error: 'Limit must be a number' })
+    .optional(),
+  offset: z.coerce
     .number({ invalid_type_error: 'Offset must be a number' })
     .optional(),
 });
